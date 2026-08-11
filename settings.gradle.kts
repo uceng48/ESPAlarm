04ccee91be0dae83e0600901195f67300791b120
uceng48 <uceng48@gmail.com>
2026-08-12 00:27:39 +0700

    Update Gradle plugins and repository settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,16 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.2.0"
+        id("org.jetbrains.kotlin.android") version "1.9.0"
+    }
 }
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } // ← penting untuk blessed-android
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "ESP32Alarm"
