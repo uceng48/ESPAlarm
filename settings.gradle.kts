a8c8b4d78852a885f148cf42bc7f449aae50595f
uceng48 <uceng48@gmail.com>
2026-08-12 00:20:55 +0700

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // ← penting untuk blessed-android
     }
 }
 rootProject.name = "ESP32Alarm"
